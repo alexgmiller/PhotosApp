6e6580c54b79d4302080642e662efa4581784f30
Alex <alexgmiller00@gmail.com>
2023-04-07 15:16:30 -0700

    Updated controller and routes

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  get 'welcome' => 'pages#home'
+
+  get "/photos" => "photos#index"
+  post "/photos" => "photos#create"
+  get "/photos:id" => "photos#show"
+  patch "/photos:id" => "photos#update"
+  delete "/photos/:id" => "photos#destroy"
+  
+end
